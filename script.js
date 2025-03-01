9e324544119dace9074189f2d89cc9c070fda493
Jacob Lilly <106789970+JLilly78@users.noreply.github.com>
2025-02-28 22:44:03 -0500

    Added themes, added beginnings of projects section
    I hope you like it

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const hiddenMenu = document.getElementById('menu-container');
+var root = document.querySelector(":root");
+var themeNameBox //= document.getElementById("theme");
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 400) {
-        hiddenMenu.classList.add('show');
-    } else {
-        hiddenMenu.classList.remove('show');
-    }
-});
+window.onload = function() {
+    themeNameBox = document.getElementById("theme");
+}
+
+function colorSet(color, accentColor, tertiaryColor1, themeName) {
+    root.style.setProperty('--pickedColor', color);
+    root.style.setProperty('--accentColor', accentColor);
+    root.style.setProperty('--tertiaryColor1', tertiaryColor1);
+    themeNameBox.textContent = themeName;
+}
